8019aa154fa27c69915c6325fe74007648bd5fba
Jorge Manrubia <jorge@hey.com>
2025-11-24 15:58:57 +0100

    Stub QB requests in tests

--- saas/test/controllers/saas_sessions_controller_test.rb
+++ saas/test/controllers/saas_sessions_controller_test.rb
@@ -5,7 +5,6 @@ class SaasSessionsControllerTest < ActionDispatch::IntegrationTest
     untenanted do
       assert_difference -> { Identity.count }, +1 do
         assert_difference -> { MagicLink.count }, +1 do
-          puts "BEFORE!"
           post session_path,
             params: { email_address: "nonexistent-#{SecureRandom.hex(6)}@example.com" },
             headers: http_basic_auth_headers("testname", "testpassword")
